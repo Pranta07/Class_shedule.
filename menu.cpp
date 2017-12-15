2140c184cb6fc4f8ea50234884b12ac6b259e4dd
Pranta07 <34563470+Pranta07@users.noreply.github.com>
2017-12-15 09:03:40 +0300

    menu_func.cpp

---
+++ menu.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void class_shedule::menu()
+ {
+    system("cls");
+    cout<<"                  *****  Personal Management System  ***** "<<endl;
+    cout<<"\n\t\t Press  b---->Build The Class Shedule Table ";
+    cout<<"\n\t\t Press  l---->To Show the full  Class Shedule Table ";
+    cout<<"\n\t\t Press  i---->Insert New Shedule        ";
+    cout<<"\n\t\t Press  d---->Delete a Shedule        ";
+    cout<<"\n\t\t Press  e---->Edit a Shedule           ";
+    cout<<"\n\t\t Press  s---->Search a Class Shedule          ";
+    cout<<"\n\t\t Press  n---->Sort The Class Shedule Table          ";
+    cout<<"\n\t\t Press  q---->Quit Program              ";
+    cout<<"\n\n\t\t     Choose WIsely!!       ";
+    cout<<"\n\n\t\tSelect Your Option Carefully ====> ";
+}
